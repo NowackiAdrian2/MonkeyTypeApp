f5d825fbd3ba1e701005653a00e3094152bea632
Adrian <nowackiadrian13@gmailcom>
2023-06-01 20:46:39 +0200

    update

--- src/main/java/typeapp/typeapp/MonkeytypeApp.java
+++ src/main/java/typeapp/typeapp/MonkeytypeApp.java
@@ -1,5 +1,9 @@
 package typeapp.typeapp;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.KeyFrame;
+import javafx.animation.PauseTransition;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
@@ -7,8 +11,10 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.File;
 
@@ -16,6 +22,8 @@ public class MonkeytypeApp extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+    Label countdownLabel;
 
     @Override
     public void start(Stage primaryStage) {
@@ -30,6 +38,27 @@ public class MonkeytypeApp extends Application {
         Button startButton = new Button("Start");
         startButton.setStyle("-fx-border-color: red; -fx-border-width: 1px; -fx-border-radius: 5px;");
         startButton.setBackground(Background.EMPTY);
+
+        Border redBorder = new Border(new BorderStroke(Color.RED, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(1)));
+        startButton.setBorder(redBorder);
+
+        // Creating the animation
+        Duration blinkDuration = Duration.seconds(1);
+        FadeTransition blinkTransition = new FadeTransition(blinkDuration, startButton);
+        blinkTransition.setFromValue(0);
+        blinkTransition.setToValue(1);
+        blinkTransition.setCycleCount(Timeline.INDEFINITE);
+        blinkTransition.setAutoReverse(true);
+
+        // Starting the animation after 5 seconds
+        PauseTransition initialDelay = new PauseTransition(Duration.seconds(5));
+        initialDelay.setOnFinished(event -> blinkTransition.play());
+        initialDelay.play();
+
+        startButton.setOnAction(event -> {
+            int selectedTime = timeChoiceBox.getValue();
+            startCountdown(selectedTime);
+        });
 
         // Create the text area
         TextArea textArea = new TextArea();
@@ -50,7 +79,8 @@ public class MonkeytypeApp extends Application {
                 if (file.isFile()) {
                     String fileName = file.getName();
                     String languageName = fileName.substring(0, fileName.lastIndexOf('.'));
-                    languageChoiceBox.getItems().add(languageName);                }
+                    languageChoiceBox.getItems().add(languageName);
+                }
             }
         }
         // Create an instance of the Controller class
@@ -79,6 +109,15 @@ public class MonkeytypeApp extends Application {
         // Create the footer VBox
         VBox footerVBox = new VBox(5, footerLabel, restartLabel, pauseLabel, endLabel);
         footerVBox.setAlignment(Pos.CENTER);
+        footerVBox.setOpacity(0);
+
+        // Create the animation to fade in the footer VBox
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(2), footerVBox);
+        fadeTransition.setFromValue(0);
+        fadeTransition.setToValue(1);
+        fadeTransition.setDelay(Duration.seconds(2));
+        // Play the animation
+        fadeTransition.play();
 
         // text box
         VBox textAreaContainer = new VBox();
@@ -101,7 +140,25 @@ public class MonkeytypeApp extends Application {
         // Set the scene
         primaryStage.setScene(scene);
         primaryStage.show();
- //       controller.startTypingPractice();
+//        controller.startTypingPractice();
+    }
+
+    private void startCountdown(int seconds) {
+        final int[] remainingSeconds = {seconds};
+        countdownLabel.setText(Integer.toString(remainingSeconds[0]));
+
+        Timeline timeline = new Timeline();
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        KeyFrame keyFrame = new KeyFrame(Duration.seconds(1), event -> {
+            remainingSeconds[0]--;
+            countdownLabel.setText(Integer.toString(remainingSeconds[0]));
+            if (remainingSeconds[0] <= 0) {
+                timeline.stop();
+                // Countdown finished, add any additional logic here
+            }
+        });
+        timeline.getKeyFrames().add(keyFrame);
+        timeline.play();
     }
 
     private Label createButtonLabel(String labelText) {
